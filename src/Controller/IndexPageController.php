ff10780db63d9ea03e160f43abbcec5f57a5de9d
My Name <my-name@chromium.org>
2019-11-07 16:54:29 +0300

    ver3

--- src/Controller/IndexPageController.php
+++ src/Controller/IndexPageController.php
@@ -4,32 +4,25 @@
 namespace App\Controller;
 
 
+use App\Service\UuidService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexPageController extends AbstractController
 {
-    public function index()
+    private $uuid;
+
+    public function __construct(UuidService $uuid)
     {
-        return $this->render('index/index.html.twig');
+        $this->uuid = $uuid;
+        return $uuid;
     }
 
-    public function getUuid()
+    public function index()
     {
-        if (function_exists('com_create_guid') === true)
-        {
-            $uuid=trim(com_create_guid(), '{}');
-            $result =$uuid;
-        }
-        else
-        {
-            $data = openssl_random_pseudo_bytes(16);
-            $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-            $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-            $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-            $result = $uuid;
-        }
+        echo $this->uuid->getUuid();
 
-        return $result;
+
+        return $this->render('index/index.html.twig');
     }
 
 }
